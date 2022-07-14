fadba6f12126a4d3e7b6a77f1b4b2d728b69bc6c
g.diasparra1 <g.diasparra1@studenti.uniba.it>
2022-07-14 16:41:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sustainabledevelopmentgoals/Goal.java
@@ -0,0 +1,6 @@
+package com.example.sustainabledevelopmentgoals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Goal extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/sustainabledevelopmentgoals/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.sustainabledevelopmentgoals;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, Home.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+}
