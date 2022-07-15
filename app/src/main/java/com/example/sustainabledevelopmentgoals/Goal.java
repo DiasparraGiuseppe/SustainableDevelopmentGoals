60eb8ab2d31181c793a28731fcc61d5d56094279
g.diasparra1 <g.diasparra1@studenti.uniba.it>
2022-07-15 12:23:36 +0200

    Tasti home cliccabili che chiamano il goal giusto

--- app/src/main/java/com/example/sustainabledevelopmentgoals/Goal.java
+++ app/src/main/java/com/example/sustainabledevelopmentgoals/Goal.java
@@ -1,6 +1,179 @@
 package com.example.sustainabledevelopmentgoals;
 
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class Goal extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal);
+
+        ConstraintLayout layout = findViewById(R.id.layoutGoal);
+
+        ImageView img_goal= (ImageView) findViewById(R.id.img_goal);
+        TextView nome_goal= (TextView) findViewById(R.id.nome_goal);
+        TextView descr_goal=findViewById(R.id.descr_goal);
+
+        Intent intent = getIntent();
+        String caller= intent.getStringExtra("caller");
+
+        Window window=this.getWindow();
+
+        switch (caller){
+            case "c1": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c1));
+                nome_goal.setText("Fine della povertà");
+                descr_goal.setText("Descrizione goal 1");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal1));
+                window.setStatusBarColor(getResources().getColor(R.color.goal1));
+                break;
+            }
+            case "c2": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c2));
+                nome_goal.setText("Stop alla fame");
+                descr_goal.setText("Descrizione goal 2");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal2));
+                window.setStatusBarColor(getResources().getColor(R.color.goal2));
+                break;
+            }
+            case "c3": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c3));
+                nome_goal.setText("Benessere e buona salute");
+                descr_goal.setText("Descrizione goal 3");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal3));
+                window.setStatusBarColor(getResources().getColor(R.color.goal3));
+                break;
+
+            }
+            case "c4": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c4));
+                nome_goal.setText("Educazione di qualità");
+                descr_goal.setText("Descrizione goal 4");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal4));
+                window.setStatusBarColor(getResources().getColor(R.color.goal4));
+                break;
+
+            }
+            case "c5": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c5));
+                nome_goal.setText("Fine della povertà");
+                descr_goal.setText("Descrizione goal 1");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal5));
+                window.setStatusBarColor(getResources().getColor(R.color.goal5));
+                break;
+            }
+            case "c6": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c6));
+                nome_goal.setText("Stop alla fame");
+                descr_goal.setText("Descrizione goal 2");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal6));
+                window.setStatusBarColor(getResources().getColor(R.color.goal6));
+                break;
+            }
+            case "c7": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c7));
+                nome_goal.setText("Benessere e buona salute");
+                descr_goal.setText("Descrizione goal 3");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal7));
+                window.setStatusBarColor(getResources().getColor(R.color.goal7));
+                break;
+            }
+            case "c8": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c8));
+                nome_goal.setText("Educazione di qualità");
+                descr_goal.setText("Descrizione goal 4");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal8));
+                window.setStatusBarColor(getResources().getColor(R.color.goal8));
+                break;
+            }
+            case "c9": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c9));
+                nome_goal.setText("Fine della povertà");
+                descr_goal.setText("Descrizione goal 1");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal9));
+                window.setStatusBarColor(getResources().getColor(R.color.goal9));
+                break;
+            }
+            case "c10": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c10));
+                nome_goal.setText("Stop alla fame");
+                descr_goal.setText("Descrizione goal 2");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal10));
+                window.setStatusBarColor(getResources().getColor(R.color.goal10));
+                break;
+
+            }
+            case "c11": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c11));
+                nome_goal.setText("Benessere e buona salute");
+                descr_goal.setText("Descrizione goal 3");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal11));
+                window.setStatusBarColor(getResources().getColor(R.color.goal11));
+                break;
+            }
+            case "c12": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c12));
+                nome_goal.setText("Educazione di qualità");
+                descr_goal.setText("Descrizione goal 4");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal12));
+                window.setStatusBarColor(getResources().getColor(R.color.goal12));
+                break;
+            }
+            case "c13": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c13));
+                nome_goal.setText("Fine della povertà");
+                descr_goal.setText("Descrizione goal 1");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal13));
+                window.setStatusBarColor(getResources().getColor(R.color.goal13));
+                break;
+            }
+            case "c14": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c14));
+                nome_goal.setText("Stop alla fame");
+                descr_goal.setText("Descrizione goal 2");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal14));
+                window.setStatusBarColor(getResources().getColor(R.color.goal14));
+                break;
+            }
+            case "c15": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c15));
+                nome_goal.setText("Benessere e buona salute");
+                descr_goal.setText("Descrizione goal 3");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal15));
+                window.setStatusBarColor(getResources().getColor(R.color.goal15));
+                break;
+            }
+            case "c16": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c16));
+                nome_goal.setText("Educazione di qualità");
+                descr_goal.setText("Descrizione goal 16");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal16));
+                window.setStatusBarColor(getResources().getColor(R.color.goal16));
+                break;
+            }
+            case "c17": {
+                img_goal.setImageDrawable(getDrawable(R.drawable.c17));
+                nome_goal.setText("Educazione di qualità");
+                descr_goal.setText("Descrizione goal 17");
+                layout.setBackgroundColor(getResources().getColor(R.color.goal17));
+                window.setStatusBarColor(getResources().getColor(R.color.goal17));
+                break;
+            }
+        }
+
+    }
 }
--- app/src/main/java/com/example/sustainabledevelopmentgoals/Home.java
+++ app/src/main/java/com/example/sustainabledevelopmentgoals/Home.java
@@ -2,6 +2,9 @@ package com.example.sustainabledevelopmentgoals;
 
 import android.os.Bundle;
 import android.view.Window;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +16,166 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Window window= this.getWindow();
         window.setStatusBarColor(this.getResources().getColor(R.color.black));
+
+        ImageView c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17;
+        c1=findViewById(R.id.c1);
+        c2=findViewById(R.id.c2);
+        c3=findViewById(R.id.c3);
+        c4=findViewById(R.id.c4);
+        c5=findViewById(R.id.c5);
+        c6=findViewById(R.id.c6);
+        c7=findViewById(R.id.c7);
+        c8=findViewById(R.id.c8);
+        c9=findViewById(R.id.c9);
+        c10=findViewById(R.id.c10);
+        c11=findViewById(R.id.c11);
+        c12=findViewById(R.id.c12);
+        c13=findViewById(R.id.c13);
+        c14=findViewById(R.id.c14);
+        c15=findViewById(R.id.c15);
+        c16=findViewById(R.id.c16);
+        c17=findViewById(R.id.c17);
+
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c1");
+                startActivity(intent);
+            }
+        });
+        c2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c2");
+                startActivity(intent);
+            }
+        });
+        c3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c3");
+                startActivity(intent);
+            }
+
+        });
+        c4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c4");
+                startActivity(intent);
+            }
+        });
+        c5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c5");
+                startActivity(intent);
+            }
+        });
+        c6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c6");
+                startActivity(intent);
+            }
+        });
+        c7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c7");
+                startActivity(intent);
+            }
+
+        });
+        c8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c8");
+                startActivity(intent);
+            }
+        });
+        c9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c9");
+                startActivity(intent);
+            }
+        });
+        c10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c10");
+                startActivity(intent);
+            }
+        });
+        c11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c11");
+                startActivity(intent);
+            }
+
+        });
+        c12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c12");
+                startActivity(intent);
+            }
+        });
+        c13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c13");
+                startActivity(intent);
+            }
+        });
+        c14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c14");
+                startActivity(intent);
+            }
+        });
+        c15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c15");
+                startActivity(intent);
+            }
+
+        });
+        c16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c16");
+                startActivity(intent);
+            }
+        });
+        c17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,Goal.class);
+                intent.putExtra("caller","c17");
+                startActivity(intent);
+            }
+        });
+
     }
 }
