4d07a21772241575e331a98f6333c43d9ca7b070
g.diasparra1 <g.diasparra1@studenti.uniba.it>
2022-07-15 12:13:31 +0200

    Inizio creazione schermata home

--- app/src/main/java/com/example/sustainabledevelopmentgoals/Home.java
+++ app/src/main/java/com/example/sustainabledevelopmentgoals/Home.java
@@ -1,6 +1,17 @@
 package com.example.sustainabledevelopmentgoals;
 
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Window window= this.getWindow();
+        window.setStatusBarColor(this.getResources().getColor(R.color.black));
+    }
 }
--- app/src/main/java/com/example/sustainabledevelopmentgoals/SplashScreen.java
+++ app/src/main/java/com/example/sustainabledevelopmentgoals/SplashScreen.java
@@ -3,6 +3,7 @@ package com.example.sustainabledevelopmentgoals;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -13,6 +14,9 @@ public class SplashScreen extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Window window= this.getWindow();
+        window.setStatusBarColor(this.getResources().getColor(R.color.white));
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
