5af69aceedc88704d019c3d2b8920bb85f19b119
g.diasparra1 <g.diasparra1@studenti.uniba.it>
2022-07-16 12:13:23 +0200

    schermata goal 2 funzionante

--- app/src/main/java/com/example/sustainabledevelopmentgoals/Goal.java
+++ app/src/main/java/com/example/sustainabledevelopmentgoals/Goal.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,13 +21,13 @@ public class Goal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_goal);
+        setContentView(R.layout.activity_goal2);
 
-        ConstraintLayout layout = findViewById(R.id.layoutGoal);
+        LinearLayout layout = findViewById(R.id.layoutGoal2);
 
-        ImageView img_goal= (ImageView) findViewById(R.id.img_goal);
-        TextView nome_goal= (TextView) findViewById(R.id.nome_goal);
-        TextView descr_goal=findViewById(R.id.descr_goal);
+        ImageView img_goal= (ImageView) findViewById(R.id.img_goal2);
+      //  TextView nome_goal= (TextView) findViewById(R.id.nome_goal);
+        TextView descr_goal=findViewById(R.id.descr_goal2);
 
         Intent intent = getIntent();
         String caller= intent.getStringExtra("caller");
@@ -35,18 +36,18 @@ public class Goal extends AppCompatActivity {
 
         switch (caller){
             case "c1": {
-                img_goal.setImageDrawable(getDrawable(R.drawable.provacanva));
-                nome_goal.setText("Fine della povertà");
+                img_goal.setImageDrawable(getDrawable(R.drawable.goal1nuovo));
+               // nome_goal.setText("Fine della povertà");
                 descr_goal.setText("Descrizione goal 1");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal1));
                 window.setStatusBarColor(getResources().getColor(R.color.goal1));
-                nome_goal.setText("Porre fine ad ogni forma di povertà");
+              //  nome_goal.setText("Porre fine ad ogni forma di povertà");
                 descr_goal.setText("Aumentare i redditi dei più poveri, garantire l'accesso ai servizi di base e proteggere tutti dai disastri naturali e causati dall'uomo");
                 break;
             }
             case "c2": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c2));
-                nome_goal.setText("Stop alla fame");
+             //   nome_goal.setText("Stop alla fame");
                 descr_goal.setText("Descrizione goal 2");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal2));
                 window.setStatusBarColor(getResources().getColor(R.color.goal2));
@@ -54,7 +55,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c3": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c3));
-                nome_goal.setText("Benessere e buona salute");
+             //   nome_goal.setText("Benessere e buona salute");
                 descr_goal.setText("Descrizione goal 3");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal3));
                 window.setStatusBarColor(getResources().getColor(R.color.goal3));
@@ -63,7 +64,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c4": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c4));
-                nome_goal.setText("Educazione di qualità");
+              //  nome_goal.setText("Educazione di qualità");
                 descr_goal.setText("Descrizione goal 4");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal4));
                 window.setStatusBarColor(getResources().getColor(R.color.goal4));
@@ -72,7 +73,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c5": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c5));
-                nome_goal.setText("Fine della povertà");
+              //  nome_goal.setText("Fine della povertà");
                 descr_goal.setText("Descrizione goal 1");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal5));
                 window.setStatusBarColor(getResources().getColor(R.color.goal5));
@@ -80,7 +81,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c6": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c6));
-                nome_goal.setText("Stop alla fame");
+               // nome_goal.setText("Stop alla fame");
                 descr_goal.setText("Descrizione goal 2");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal6));
                 window.setStatusBarColor(getResources().getColor(R.color.goal6));
@@ -88,7 +89,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c7": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c7));
-                nome_goal.setText("Benessere e buona salute");
+             //   nome_goal.setText("Benessere e buona salute");
                 descr_goal.setText("Descrizione goal 3");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal7));
                 window.setStatusBarColor(getResources().getColor(R.color.goal7));
@@ -96,7 +97,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c8": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c8));
-                nome_goal.setText("Educazione di qualità");
+              //  nome_goal.setText("Educazione di qualità");
                 descr_goal.setText("Descrizione goal 4");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal8));
                 window.setStatusBarColor(getResources().getColor(R.color.goal8));
@@ -104,7 +105,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c9": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c9));
-                nome_goal.setText("Fine della povertà");
+             //   nome_goal.setText("Fine della povertà");
                 descr_goal.setText("Descrizione goal 1");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal9));
                 window.setStatusBarColor(getResources().getColor(R.color.goal9));
@@ -112,7 +113,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c10": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c10));
-                nome_goal.setText("Stop alla fame");
+              //  nome_goal.setText("Stop alla fame");
                 descr_goal.setText("Descrizione goal 2");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal10));
                 window.setStatusBarColor(getResources().getColor(R.color.goal10));
@@ -121,7 +122,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c11": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c11));
-                nome_goal.setText("Benessere e buona salute");
+             //   nome_goal.setText("Benessere e buona salute");
                 descr_goal.setText("Descrizione goal 3");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal11));
                 window.setStatusBarColor(getResources().getColor(R.color.goal11));
@@ -129,7 +130,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c12": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c12));
-                nome_goal.setText("Educazione di qualità");
+             //   nome_goal.setText("Educazione di qualità");
                 descr_goal.setText("Descrizione goal 4");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal12));
                 window.setStatusBarColor(getResources().getColor(R.color.goal12));
@@ -137,7 +138,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c13": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c13));
-                nome_goal.setText("Fine della povertà");
+           //     nome_goal.setText("Fine della povertà");
                 descr_goal.setText("Descrizione goal 1");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal13));
                 window.setStatusBarColor(getResources().getColor(R.color.goal13));
@@ -145,7 +146,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c14": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c14));
-                nome_goal.setText("Stop alla fame");
+             //   nome_goal.setText("Stop alla fame");
                 descr_goal.setText("Descrizione goal 2");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal14));
                 window.setStatusBarColor(getResources().getColor(R.color.goal14));
@@ -153,7 +154,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c15": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c15));
-                nome_goal.setText("Benessere e buona salute");
+               // nome_goal.setText("Benessere e buona salute");
                 descr_goal.setText("Descrizione goal 3");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal15));
                 window.setStatusBarColor(getResources().getColor(R.color.goal15));
@@ -161,7 +162,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c16": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c16));
-                nome_goal.setText("Educazione di qualità");
+              //  nome_goal.setText("Educazione di qualità");
                 descr_goal.setText("Descrizione goal 16");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal16));
                 window.setStatusBarColor(getResources().getColor(R.color.goal16));
@@ -169,7 +170,7 @@ public class Goal extends AppCompatActivity {
             }
             case "c17": {
                 img_goal.setImageDrawable(getDrawable(R.drawable.c17));
-                nome_goal.setText("Educazione di qualità");
+            //    nome_goal.setText("Educazione di qualità");
                 descr_goal.setText("Descrizione goal 17");
                 layout.setBackgroundColor(getResources().getColor(R.color.goal17));
                 window.setStatusBarColor(getResources().getColor(R.color.goal17));
